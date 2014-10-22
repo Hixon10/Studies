c397f770df89eab9023e954f5278187a88dda5c0
ayakomarov <a.ya.komarov@gmail.com>
2014-10-22 15:25:09 +0400

    change: floodFill

--- ComputerVision/tasks1-2/task2.py
+++ ComputerVision/tasks1-2/task2.py
@@ -1,3 +1,4 @@
+import cv
 import cv2
 import numpy as np
 
@@ -11,12 +12,20 @@ img = cv2.erode(img,kernel,iterations = 1)
 kernel = np.ones((3,5),np.uint8)
 img = cv2.dilate(img, kernel, iterations = 1)
 
-(contours, _) = cv2.findContours(img.copy(), cv2.cv.CV_RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-for c in contours:
-	x,y,w,h = cv2.boundingRect(c)
-	cv2.rectangle(startImg,(x,y),(x+w,y+h),(0,255,0),1)
-	cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+flooded = img.copy()
+cols, rows = flooded.shape
+mask = np.zeros((cols+2, rows+2), np.uint8)
+    
+for y in range(rows):
+	for x in range(cols):
+		pix = flooded[x,y]
+		seed_pt = y, x
+		if pix == 0:
+			continue
+		retval, rect = cv2.floodFill(flooded, mask, seed_pt, 0, 0, 0, 
+			4 | cv2.FLOODFILL_FIXED_RANGE)
+		x,y,w,h = rect
+		cv2.rectangle(startImg,(x,y),(x+w,y+h),(0,255,0),1)
 
 cv2.imwrite("binaryWords.bmp", img) 	
 cv2.imwrite("detectWords.bmp", startImg) 
